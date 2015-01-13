658f2446893a4acb7faf127bcb49bda9cd2635e1
Ryan VanGundy <ryan.vangundy@gmail.com>
2015-01-12 19:01:08 -0500

    Expand test site

--- src/build.js
+++ src/build.js
@@ -44,32 +44,15 @@ function registerPartials(partials, partialPath) {
 }
 
 /**
- * Populates a site object with the path to each HTML file. Traverses
- * the tree recursively, providing a flattened path to each site.
- * @param {Object} site A site object
- * @param {Object} files An object containing all files
- * @param {Array} path The path to the current location in the tree
+ * 
+ * @param {type} [name] [description]
  */
-function populateSite(site, files, sitePath) {
-    sitePath = sitePath || [];
+function processSite(files, site) {
+    site = site || {};
 
     for (var file in files) {
 
-        // Fill in index.html
-        if (file === 'index.hbs') {
-            sitePath.push('index.html');
-            site[sitePath.join('/')] = [];
-            sitePath.pop();
-        }
-
-        // Recursively continue populating
-        else if (files[file] === Object(files[file])) {
-            sitePath.push(file);
-            populateSite(site, files[file], sitePath);
-        }
     }
-
-    sitePath.pop();
 }
 
 /**************
@@ -77,28 +60,19 @@ function populateSite(site, files, sitePath) {
  **************/
 
 module.exports = function build(src, dest) {
+    var site;
     var t0 = Date.now();
-    var site = {}; 
 
     // Read the entire src tree
     readDirFiles.read(src, 'utf8', function(err, files) {
+        console.log(Date.now() - t0);
 
-        // Populate the site object
-        populateSite(site, files.site);
+        console.log(JSON.stringify(files));
 
         // Add partials
         registerPartials(files.partials);
 
-        // Register module helper
-        moduleHelper(moduleName, moduleList, files);
-
-        // // Compile templates
-        
-
-        // files.forEach(function(file) {
-        //     if (path.basename(file) === 'index.hbs') {
-        //         var template = handlebars.compile
-        //     }
-        // });
+        // Process site
+        site = processSite(files.site);
     });
 };
---
+++ src/preprocess-site.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Preprocesses the site object
+ * @param {type} [name] [description]
+ */
+function preprocess(files) {
+    
+}
+
+module.exports = function (siteFolder) {
+
+};
