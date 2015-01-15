b020557d9682c8221401a74b13fd6e633ea15fca
Ryan VanGundy <ryan.vangundy@gmail.com>
2015-01-15 14:59:22 -0500

    Remove no-js class

--- src/client-runmodules.js
+++ src/client-runmodules.js
@@ -4,6 +4,10 @@ window.addEventListener('load', function runModules() {
     var mod;
     var moduleName;
     var modules = window.document.querySelectorAll('[data-module]');
+    var docElement = document.documentElement;
+
+    // Remove no-js class
+    docElement.className = docElement.className.replace(/(^|\s)no-js(\s|$)/, '$1$2');
 
     // Run module scripts against each module element
     for (var i=0, len=modules.length; i<len; i+=1) {
