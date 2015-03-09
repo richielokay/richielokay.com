0178aa61e0547b8dcc4a49cf7d450dc863705cd3
Ryan VanGundy <ryan.vangundy@gmail.com>
2015-03-09 18:01:17 -0400

    Fix scss source maps

--- src/server/static-server.js
+++ src/server/static-server.js
@@ -19,29 +19,37 @@ module.exports = function(context) {
     var settings = context.settings;
     var servePath = path.join(process.cwd(), settings.dest);
 
-    return new Promise(function(resolve) {
+    return new Promise(function(resolve, reject) {
 
-        // Do nothing if no server is configured
-        if (!settings.server) {
-            resolve(context);
-            return;
+        try {
+            // Do nothing if no server is configured
+            if (!settings.server) {
+                resolve(context);
+                return;
+            }
+
+            // Set up the static server
+            port = settings.server.port;
+            appServer = new staticServer.Server(servePath, {
+                gzip: settings.gzip,
+                cache: 0
+            });
+
+            // Set up the http server
+            context.server = http.createServer(function(request, response) {
+                request.addListener('end', function() {
+
+                    // Make sure to send source maps appropriately
+                    if (path.extname(request.url) === '.map') {
+                        appServer.serveFile(request.url, 200, {}, request, response);
+                    } else { appServer.serve(request, response); }
+                }).resume();
+            }).listen(port, function() {
+                log('Server', 'Serving at http://localhost:' + port);
+                resolve(context);
+            });
+        } catch(err) {
+            reject('[static-server.js] ' + err);
         }
-
-        // Set up the static server
-        port = settings.server.port;
-        appServer = new staticServer.Server(servePath, {
-            gzip: settings.gzip,
-            cache: 0
-        });
-
-        // Set up the http server
-        context.server = http.createServer(function(request, response) {
-            request.addListener('end', function() {
-                appServer.serve(request, response);
-            }).resume();
-        }).listen(port, function() {
-            log('Server', 'Serving at http://localhost:' + port);
-            resolve(context);
-        });
     });
 };
